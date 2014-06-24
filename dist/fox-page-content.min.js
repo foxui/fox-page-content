18439b1a1808bf9f66ae371a6b713bfd6ecb0843
meng <30962088@qq.com>
2014-06-24 20:22:49 +0800

    update

---
+++ dist/fox-page-content.min.js
@@ -0,0 +1,9 @@
+/*!
+ * =====================================================
+ * Foxui v0.1.0 (https://github.com/foxui/fox-page-content)
+ * Copyright 2014 fex-team
+ * Licensed under BSD (https://github.com/foxui/fox-page-content/blob/master/LICENSE)
+ *
+ * v0.1.0 designed by @fex-team.
+ * =====================================================
+ */
